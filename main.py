c41a0a044a3899c84f0628b849b07224a6bc9271
Moustapha <Moustaphabah2005@gmail.com>
2026-04-09 11:47:17 -0400

    imported helpers.py and data.py to main.py, added all 9 methods to TestUrbanRoutes class including setup_class

--- main.py
+++ main.py
@@ -1,2 +1,56 @@
+import data
+import helpers
+from data import URBAN_ROUTES_URL
+from helpers import is_url_reachable
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if is_url_reachable(URBAN_ROUTES_URL):
+            print('Connected to Urban Routes Server')
+        else:
+            print('Cannot connect to Urban Routes. Check the server is on and still running')
+
+    def test_set_route(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print('function created for set route')
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Add in S8
+        for i in range(2):
+            # Add in S8
+            pass
+        print('function created for set route')
+        pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print('function created for set route')
+        pass
