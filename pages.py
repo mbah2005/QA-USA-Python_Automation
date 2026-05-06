6e38df93213abc0b3fdb91d0288dd295fa47d678
Moustapha <Moustaphabah2005@gmail.com>
2026-05-06 13:37:50 -0400

    combined methods to make main page easier to read

--- pages.py
+++ pages.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from helpers import retrieve_phone_code
 
 class UrbanRoutesPage:
     # Locators as class attributes
@@ -66,12 +67,12 @@ class UrbanRoutesPage:
         self.driver.find_element(*self.CONFIRM_BUTTON_LOCATOR).click()
 
 
-    def click_payment_method(self, last_name):
+    def click_payment_method(self):
         # Enter Last Name
         self.driver.find_element(*self.PAYMENT_METHOD_LOCATOR).click()
 
 
-    def add_card(self):
+    def click_add_card(self):
         # Enter Date of Birth
         self.driver.find_element(*self.ADD_CARD_LOCATOR).click()
 
@@ -109,7 +110,83 @@ class UrbanRoutesPage:
     def click_counter_button(self):
         # Click Add Button
         self.driver.find_element(*self.COUNTER_LOCATOR).click()
-        
-    def get_verification_text(self):
-        # Return the verification text
-        return self.driver.find_element(*self.VERIFICATION_TEXT_LOCATOR).text
+
+    def click_order_button(self):
+        # Click Add Button
+        self.driver.find_element(*self.CONFIRM_BUTTON_LOCATOR).click()
+
+
+
+
+    def enter_locations(self, from_text, to_text):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        #Retrieve the displayed values and assert that they match the input.
+
+    def select_supportive_plan(self, from_text, to_text):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_call_taxi()
+        self.click_supportive_icon()
+        #Retrieve the active selection value. Assert that the correct plan is selected dynamically.
+
+
+    def input_phone_number(self, from_text, to_text, phone_number, sms):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_call_taxi()
+        self.click_supportive_icon()
+        self.click_phone_number()
+        self.enter_phone_number(phone_number)
+        retrieve_phone_code(sms)
+        self.enter_code(sms)
+        self.click_confirm_button()
+        #Assert that the user's phone number is reflected properly in the phone field
+
+    def add_credit_card(self, from_text, to_text, card_number, card_code):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_call_taxi()
+        self.click_supportive_icon()
+        self.click_payment_method()
+        self.click_add_card()
+        self.enter_card_number(card_number)
+        self.enter_card_code(card_code)
+        self.click_add_card()
+        self.click_link_button()
+        self.click_close_button()
+        # Assert text in payment method is “Card”
+
+    def add_comment_for_driver(self, from_text, to_text, comment):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_call_taxi()
+        self.click_supportive_icon()
+        self.click_comment_button()
+        self.enter_comment(comment)
+        # Retrieve and assert that the message is stored correctly.
+
+    def add_blanket_and_handkerchief(self, from_text, to_text):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_call_taxi()
+        self.click_supportive_icon()
+        self.click_checkbox_button()
+        # Verify that the selection is confirmed using the correct assertion.
+
+    def add_ice_cream(self, from_text, to_text):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
+        self.click_call_taxi()
+        self.click_supportive_icon()
+        # Assert that the displayed count matches 2.
+
+    def order_taxi(self, from_text, to_text, comment, phone_number, sms):
+        self.input_phone_number(from_text, to_text, phone_number, sms)
+        self.enter_comment(comment)
+        self.click_order_button()
+        # Assert that the car search modal appears.
+
+
+
+
