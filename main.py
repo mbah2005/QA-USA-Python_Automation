4ca1beeaaf351949b7652f8822b6ab82c22c86a9
Moustapha <Moustaphabah2005@gmail.com>
2026-05-12 00:30:01 -0400

    changed imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from data import URBAN_ROUTES_URL, ADDRESS_FROM, ADDRESS_TO, PHONE_NUMBER, CARD_NUMBER, CARD_CODE, MESSAGE_FOR_DRIVER
-from helpers import is_url_reachable
+import data
+import helpers
 from selenium import webdriver
 import time
 from pages import UrbanRoutesPage
@@ -15,7 +15,7 @@ class TestUrbanRoutes:
         cls.driver = webdriver.Chrome()
 
         # Check if the application server is reachable before starting tests
-        if is_url_reachable(URBAN_ROUTES_URL):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print('Connected to Urban Routes Server')
         else:
             print('Cannot connect to Urban Routes. Check the server is on and still running')
@@ -23,49 +23,49 @@ class TestUrbanRoutes:
     def test_set_route(self):
         # Tests that the 'From' and 'To' addresses are correctly entered and displayed
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.enter_locations(ADDRESS_FROM, ADDRESS_TO)
-        assert main_page.get_from_address_text() == ADDRESS_FROM
-        assert main_page.get_to_address_text() == ADDRESS_TO
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        assert main_page.get_from_address_text() == data.ADDRESS_FROM
+        assert main_page.get_to_address_text() == data.ADDRESS_TO
 
 
     def test_select_plan(self):
         # Verifies that the 'Supportive' tariff plan can be selected
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.select_supportive_plan(ADDRESS_FROM, ADDRESS_TO)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.select_supportive_plan(data.ADDRESS_FROM, data.ADDRESS_TO)
         assert 'Supportive' in main_page.get_supportive_text()
 
 
     def test_fill_phone_number(self):
         # Confirms that the user's phone number is correctly populated in the profile
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.input_phone_number(ADDRESS_FROM, ADDRESS_TO, PHONE_NUMBER)
-        assert main_page.get_phone_number() == PHONE_NUMBER
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.input_phone_number(data.ADDRESS_FROM, data.ADDRESS_TO, data.PHONE_NUMBER)
+        assert main_page.get_phone_number() == data.PHONE_NUMBER
 
 
     def test_fill_card(self):
         # Validates the process of adding a credit card as a payment method
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.add_credit_card(ADDRESS_FROM, ADDRESS_TO, CARD_NUMBER, CARD_CODE)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.add_credit_card(data.ADDRESS_FROM, data.ADDRESS_TO, data.CARD_NUMBER, data.CARD_CODE)
         assert 'Card' in main_page.get_payment_method()
 
 
     def test_comment_for_driver(self):
         # Tests the ability to leave a text comment/message for the taxi driver
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.add_comment_for_driver(ADDRESS_FROM, ADDRESS_TO, MESSAGE_FOR_DRIVER)
-        assert main_page.get_comment() == MESSAGE_FOR_DRIVER
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.add_comment_for_driver(data.ADDRESS_FROM, data.ADDRESS_TO, data.MESSAGE_FOR_DRIVER)
+        assert main_page.get_comment() == data.MESSAGE_FOR_DRIVER
 
 
     def test_order_blanket_and_handkerchiefs(self):
         # Verifies that the optional 'blanket and handkerchiefs' item can be toggled
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.add_blanket_and_handkerchief(ADDRESS_FROM, ADDRESS_TO)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.add_blanket_and_handkerchief(data.ADDRESS_FROM, data.ADDRESS_TO)
         assert main_page.verify_checkbox()
 
 
@@ -73,8 +73,8 @@ class TestUrbanRoutes:
         # Tests the counter functionality by ordering exactly 2 ice creams
         number_of_ice_creams = 2
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.add_ice_cream(ADDRESS_FROM, ADDRESS_TO)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.add_ice_cream(data.ADDRESS_FROM, data.ADDRESS_TO)
 
         # Loop to click the increment button based on the required quantity
         for i in range(number_of_ice_creams):
@@ -86,8 +86,8 @@ class TestUrbanRoutes:
     def test_car_search_model_appears(self):
         # Final flow test: triggers the order and checks if the car search modal/overlay appears
         main_page = UrbanRoutesPage(self.driver)
-        self.driver.get(URBAN_ROUTES_URL)
-        main_page.order_taxi(ADDRESS_FROM, ADDRESS_TO, PHONE_NUMBER, MESSAGE_FOR_DRIVER)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        main_page.order_taxi(data.ADDRESS_FROM, data.ADDRESS_TO, data.PHONE_NUMBER, data.MESSAGE_FOR_DRIVER)
         assert main_page.get_car_search()
 
 
